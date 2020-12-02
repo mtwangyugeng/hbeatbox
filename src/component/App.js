11a9f1b4f195dc7724ec030f0e231e050bbd0509
mtwangyugeng <mtwangyugeng@gmail.com>
2020-12-02 12:56:12 -0500

    decide on architechture

--- src/component/App.js
+++ src/component/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/* 
+App Contains all of the beat box components.
+1. the control bar
+2. the rhythm square panel
+*/ 
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      metastate: null
+    }
+  }
+  render() {
+    return ( 
+    <div className = "ccc">
+      this is app
+      </div >
+      );
+  }
 }
 
 export default App;
