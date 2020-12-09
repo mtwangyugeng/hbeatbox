18bc9b7d8427904de51b5ec493e1812fab0f5c75
Simon Dickenson <mtwangyugeng@gmail.com>
2020-12-09 18:08:10 -0500

    better css

--- src/component/RhythmColumn.js
+++ src/component/RhythmColumn.js
@@ -3,12 +3,12 @@ import PropTypes from "prop-types";
 
 import RhythmSquare from './RhythmSquare';
 import './RhythmColumn.css';
-import ccc from './ccc.wav'
-import ccd from './ccd.wav'
-import cce from './cce.wav'
-import ccf from './ccf.wav'
-import ccg from './ccg.wav'
-import cch from './cch.wav'
+import ccc from './sounds/ccc.wav'
+import ccd from './sounds/ccd.wav'
+import cce from './sounds/cce.wav'
+import ccf from './sounds/ccf.wav'
+import ccg from './sounds/ccg.wav'
+import cch from './sounds/cch.wav'
 
 class RhythmColumn extends React.Component{
   //CONST
@@ -31,7 +31,7 @@ class RhythmColumn extends React.Component{
               <RhythmSquare activated= {this.state.activated || (this.props.act_col === this.props.colnum)?1:0 } sound = {v}></RhythmSquare>
             )
           }
-          <button onClick = {this.updateState}>{this.props.act_col}</button>
+          <div className = {"testcol"} onClick = {this.updateState}>{this.props.act_col}</div>
       </div >
     );
   }
