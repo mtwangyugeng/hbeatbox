24bf1bf504dc30d9cb342a1c5a2e244924a2e821
Simon Dickenson <mtwangyugeng@gmail.com>
2020-12-18 21:27:00 -0500

    browser dont support capturing audios??

--- src/component/RhythmVisualizer.js
+++ src/component/RhythmVisualizer.js
@@ -8,6 +8,7 @@ export default class RhythmVisualizer extends React.Component{
     
     audioData: new Uint8Array(0)
   }
+  canvas = React.createRef();
 
   componentDidMount() {
     this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
@@ -31,11 +32,36 @@ export default class RhythmVisualizer extends React.Component{
     this.rafId = requestAnimationFrame(this.tick);
   }
 
+  draw() {
+    const canvas = this.canvas.current;
+    const height = canvas.height;
+    const width = canvas.width;
+    const context = canvas.getContext('2d');
+    let x = 0;
+    const sliceWidth = (width * 1.0) / this.state.audioData.length;
+    context.lineWidth = 2;
+    context.strokeStyle = '#000000';
+    context.clearRect(0, 0, width, height);
+    context.beginPath();
+    context.moveTo(0, height / 2);
+    for (const item of this.state.audioData) {
+      const y = (item / 255.0) * height;
+      context.lineTo(x, y);
+      x += sliceWidth;
+    }
+    context.lineTo(x, height / 2);
+    context.stroke();
+  }
+
+  componentDidUpdate() {
+    this.draw();
+  }
+
   render() {
       return ( 
-          <div className = "visual-box">
-            <textarea value={this.state.audioData} />;
-          </div>
+          <canvas className = "visual-box" ref={this.canvas}>
+          
+          </canvas>
       );
   }
 
